f82a24b688b756795f4f8f030db61ce83a5cd5ce
Lyxx <rg.gamiiing233@gmail.com>
2023-12-05 14:46:18 +0700

    ppp

--- src/Model/ModelUser.java
+++ src/Model/ModelUser.java
@@ -1,34 +1,34 @@
 package Model;
+
 import ModelJSON.ModelJSONUser;
 import Node.NodeUser;
 
 import java.util.ArrayList;
 
 public class ModelUser {
-   // public static NodeAdmin admin = new NodeAdmin();
     private ArrayList<NodeUser> datausers = new ArrayList<>();
-
+    private ModelJSONUser modelJSONUser = new ModelJSONUser();
 
     public ModelUser() {
         datausers = new ArrayList<>();
     }
 
-    public void RegisterUser(String uname , String pass){
-        datausers.add(new NodeUser(uname , pass));
+    public void RegisterUser(String uname, String pass) {
+        datausers.add(new NodeUser(uname, pass));
+        modelJSONUser.tulisUserKeJSON(new NodeUser(uname, pass));
     }
 
-    public void ViewAllUserSewa(){
-        ArrayList <NodeUser> listuser = new ModelJSONUser().readFromJSON();
-        if (listuser != null){
-            for (NodeUser user : listuser){
+    public void ViewAllUserSewa() {
+        ArrayList<NodeUser> listuser = modelJSONUser.readFromJSON{}
+        if (listuser != null) {
+            for (NodeUser user : listuser) {
                 user.ViewUserSewa();
                 System.out.println("===================");
             }
         }
     }
-    public ArrayList<NodeUser>getListDatausers(){
+
+    public ArrayList<NodeUser> getListDatausers() {
         return this.datausers;
     }
 }
-
-
--- src/View/MenuUser.java
+++ src/View/MenuUser.java
@@ -1,56 +1,56 @@
 package View;
 
 import Model.ModelUser;
+import ModelJSON.ModelJSONUser;
+import Node.NodeUser;
 
 import java.util.Scanner;
-import View.warna;
-
-import javax.swing.*;
 
 public class MenuUser {
     static ModelUser cUser = new ModelUser();
+    static ModelJSONUser modelJSONUser = new ModelJSONUser();
     public static Scanner input = new Scanner(System.in);
+
     public static void ListMenuUser() {
         int plh;
         do {
             System.out.println(" ============================== ");
             System.out.println("           Menu User ");
             System.out.println(" ============================== ");
-            System.out.println(warna.color_green +"\n 1. Buat Akun  \n 2. Login \n 3. Kembali"+ warna.text_reset);
+            System.out.println(warna.color_green + "\n 1. Buat Akun  \n 2. Login \n 3. Kembali" + warna.text_reset);
             System.out.print(" Pilih Opsi : ");
 
             plh = input.nextInt();
             input.nextLine();
 
-
-
             if (plh == 1) {
-                System.out.println(warna.color_purple+"--- Registrasi Akun ---"+warna.text_reset);
-                System.out.print(warna.color_blue+"Masukkan username : ");
+                System.out.println(warna.color_purple + "--- Registrasi Akun ---" + warna.text_reset);
+                System.out.print(warna.color_blue + "Masukkan username : ");
                 String username = input.nextLine();
                 System.out.print("Masukkan password : ");
                 String password = input.nextLine();
                 cUser.RegisterUser(username, password);
-                System.out.println(warna.color_green+"Akun berhasil didaftarkan"+warna.text_reset);
+                modelJSONUser.tulisUserKeJSON(new NodeUser(username, password));
+                System.out.println(warna.color_green + "Akun berhasil didaftarkan" + warna.text_reset);
                 ListMenuUser();
             } else if (plh == 2) {
-                System.out.print(warna.color_blue+"Masukkan username : ");
+                System.out.print(warna.color_blue + "Masukkan username : ");
                 String username = input.nextLine();
                 System.out.print("Masukkan password : ");
                 String password = input.nextLine();
-                for (int i = 0; i < cUser.getListDatausers().size(); i++) {
-                    if (cUser.getListDatausers().get(i).uname.equals(username)) {
-                        if (cUser.getListDatausers().get(i).pass.equals(password)) {
-                            System.out.println("Anda berhasil login sebagai user");
-                            MenuUser.ListMenuUser();
-                        }
-                    }
+                
+                NodeUser penggunaMasuk = modelJSONUser.loginPenggunaDariJSON(username, password);
+                
+                if (penggunaMasuk != null) {
+                    System.out.println("Anda berhasil login sebagai pengguna");
+                    MenuUser.ListMenuUser();
+                } else {
+                    System.out.println("Username atau password salah");
                 }
-
             } else {
                 Main.main(null);
                 break;
             }
-        }
+        } while (plh != 3);
     }
 }
